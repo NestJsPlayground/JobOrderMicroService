538398666624a74d1111d82f799e99c16571e49f
Tilen Tomakić <tilen@tomakic.com>
2017-12-13 15:19:41 +0100

    Remove model.

--- services/src/main/java/si/fri/rso/samples/orders/services/OrdersBean.java
+++ services/src/main/java/si/fri/rso/samples/orders/services/OrdersBean.java
@@ -2,7 +2,6 @@ package si.fri.rso.samples.orders.services;
 
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import com.kumuluz.ee.rest.utils.JPAUtils;
-import si.fri.rso.samples.orders.Order;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
