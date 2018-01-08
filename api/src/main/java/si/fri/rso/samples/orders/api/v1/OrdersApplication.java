fb685130b22a18d7ba161af99a5ec1436a400d95
Tilen Tomakić <tilen@tomakic.com>
2018-01-08 01:47:30 +0100

    Switch to consul.

--- api/src/main/java/si/fri/rso/samples/orders/api/v1/OrdersApplication.java
+++ api/src/main/java/si/fri/rso/samples/orders/api/v1/OrdersApplication.java
@@ -6,6 +6,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
-@RegisterService
+@RegisterService(value = "rso-order", ttl = 20, pingInterval = 15)
 public class OrdersApplication extends Application {
 }
--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-gcloud container clusters get-credentials "fri-cluster" --zone "europe-west3-c"
-
-kubectl apply -f mongo-statefulset.yaml
-kubectl apply -f mongo-service.yaml
-
-kubectl apply -f app-statefulset.yaml
-kubectl apply -f app-service.yaml
-
-kubectl apply -f consul-service.yaml
-kubectl apply -f consul-statefulset.yaml
+kubectl apply -f order-deployment.yaml
+kubectl apply -f order-service.yaml
